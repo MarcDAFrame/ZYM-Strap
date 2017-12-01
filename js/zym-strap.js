0d8e27a83ae8e42724f828c374edd20a5efc65c1
dcti <mframe45@gmail.com>
2017-12-01 17:52:40 -0500

    Pushing my changes for v0.1

--- js/zym-strap.js
+++ js/zym-strap.js
@@ -147,29 +147,60 @@ function slideshow(){
 
 function transit_loader(){
     transitions = [
+        //["transition-class-name", "transit-type", "default-value", "return value"]
         [".transit-pop", 'scale', '1.05', {scale:'1'}],
         [".transit-slide-x",  'x', '-5', {x:'0'}],
         [".transit-slide-y", 'y', '-5', {y:'0'}],     
+        [".transit-rotate", 'rotate', '15deg', {rotate:'0deg'}],     
+        [".transit-skew-x", 'skewX', '15deg', {skewX:'0deg'}],     
+        [".transit-skew-y", 'skewY', '15deg', {skewY:'0deg'}],     
+        
     ]
     $.each(transitions, function(id, attribute){
-        $(attribute[0]).each( function(index){        
-            $(this).hover(function(){  
-                // console.log($(this).attr('value'))
-                value = $(this).attr('value')
-                let dict = new Array()
+        $(attribute[0]).each( function(index){     
+            type = $(this).attr('type')
+            if (type == 'hover'){
+                $(this).hover(function(){  
+                    // console.log($(this).attr('value'))
+                    value = $(this).attr('value')
+                    let dict = new Array()
+    
+                    if(value){
+                        dict[attribute[1]] = value
+                        $(this).transition(dict);  
+                    }else{value
+                        dict[attribute[1]] = attribute[2]                    
+                        $(this).transition(dict);                       
+                    }
+                    
+                },
+                function(){
+                    $(this).transition(attribute[3]);            
+                })
+            }else if(type == 'click'){
+                let clicked = false                            
+                $(this).click(function(){  
+                    value = $(this).attr('value')
+                    // console.log($(this).attr('value'))
+                    let dict = new Array()
+                    if (clicked){
+                        clicked = false
+                        $(this).transition(attribute[3]);                                    
+                    }else{
+                        clicked = true
+                        if(value){
+                            dict[attribute[1]] = value
+                            $(this).transition(dict);  
+                        }else{value
+                            dict[attribute[1]] = attribute[2]                    
+                            $(this).transition(dict);                       
+                        }
+                    }
 
-                if(value){
-                    dict[attribute[1]] = value
-                    $(this).transition(dict);  
-                }else{value
-                    dict[attribute[1]] = attribute[2]                    
-                    $(this).transition(dict);                       
-                }
-                
-            },
-            function(){
-                $(this).transition(attribute[3]);            
-            })
+                    
+                })
+            }    
+
             
         })
     })
@@ -199,10 +230,31 @@ function dropdown(){
 
 }
 
+function faq(){
+    $('.faq-snap').on('click', function(){
+        let content = $(this).next()[0];
+        if (content.style.display === "block") {
+            content.style.display = "none";
+        } else {
+            content.style.display = "block";
+        }
+    });
+    $('.faq').on('click', function(){
+
+        let content = $(this).next()[0];
+
+        if (content.style.maxHeight){
+            content.style.maxHeight = null;
+        }else{
+            content.style.maxHeight = String(content.scrollHeight)+"px";
+        }
+    });
+}
 $(document).ready(function() {
     slideshow();
     preload();
     modals();
     transit_loader();
     dropdown();
+    faq();
 })
